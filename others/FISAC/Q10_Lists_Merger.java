252d4a6229789d28f1289bc119db232601324966
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-11-01 20:06:10 +0530

    Update FISAC

--- others/FISAC/Q10_Lists_Merger.java
+++ others/FISAC/Q10_Lists_Merger.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 class ListOps {
-    static List<Integer> predicateTest(List<Integer> list) {
-        List<Integer> list_new;
+    static ArrayList<Integer> predicateTest(ArrayList<Integer> list) {
+        ArrayList<Integer> list_new;
         int len;
         Predicate<Integer> predicateIsEven;
 
         len = list.size();
         if (len == 0)
-            return null;
+            return new ArrayList<>();
 
         predicateIsEven = i -> (i % 2 == 0);
-        list_new = new ArrayList<>(len);
+        list_new = new ArrayList<Integer>(len);
 
         for (int i = 0; i < len; i++)
             if (predicateIsEven.test(list.get(i)))
@@ -23,15 +23,15 @@ class ListOps {
         return list_new;
     }
 
-    static <T> List<T> merge(List<T> a, List<T> b) {
+    static <T> ArrayList<T> merge(ArrayList<T> a, ArrayList<T> b) {
         int len_a, len_b, n, i, j, k;
-        List<T> merged;
+        ArrayList<T> merged;
 
         len_a = a.size();
         len_b = b.size();
 
         if (len_a == 0 && len_b == 0)
-            return null;
+            return new ArrayList<>();
         else if (len_a == 0)
             return new ArrayList<>(b);
         else if (len_b == 0)
@@ -48,11 +48,11 @@ class ListOps {
         }
 
         // adding trailing elements
-        for (; j < len_a; j++)
-            merged.add(a.get(j));
+        while (j < len_a)
+            merged.add(a.get(j++));
 
-        for (; k < len_b; k++)
-            merged.add(b.get(k));
+        while (k < len_b)
+            merged.add(b.get(k++));
 
         return merged;
     }
@@ -63,8 +63,11 @@ class Q10_Lists_Merger {
         Scanner sc = new Scanner(System.in);
         int n1, n2;
 
-        ArrayList<Integer> arr1 = new ArrayList<Integer>();
-        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        // Integer demonstration
+        ArrayList<Integer> arr1, arr2, predicateSatisfied, merged;
+
+        arr1 = new ArrayList<Integer>();
+        arr2 = new ArrayList<Integer>();
 
         System.out.print("\nEnter length of array 1 and array 2: ");
         n1 = sc.nextInt();
@@ -78,25 +81,40 @@ class Q10_Lists_Merger {
         for (int i = 0; i < n2; i++)
             arr2.add(sc.nextInt());
 
-        List<Integer> predicateSatisfied = ListOps.predicateTest(arr1);
-
+        predicateSatisfied = ListOps.predicateTest(arr1);
         System.out.println("\nElements satisfying predicate, i.e. are even:");
         for (Integer i : predicateSatisfied)
             System.out.print(i + " ");
-        System.out.println();
 
-        List<Integer> merged = ListOps.merge(arr1, arr2);
+        merged = ListOps.merge(arr1, arr2);
 
-        System.out.println("\nAfter alternate merging:");
+        System.out.println("\n\nAfter alternate merging:");
         for (Integer i : merged)
             System.out.print(i + " ");
         System.out.println();
 
-        // // throws compile time error due to type mismatch
+        // // compile thrown in case of type mismatch
         // ArrayList<String> arr3 = new ArrayList<String>();
         // arr3.add("abc");
         // arr3.add("def");
         // List<Integer> merged = ListOps.merge(arr1, arr3);
+
+        // strings demonstration
+        ArrayList<String> arr3, arr4, merged_strings;
+
+        arr3 = new ArrayList<String>();
+        arr4 = new ArrayList<String>();
+        arr3.add("abc");
+        arr3.add("ghi");
+        arr4.add("def");
+        arr4.add("jkl");
+
+        merged_strings = ListOps.merge(arr3, arr4);
+
+        System.out.println("\nStrings after alternate merging:");
+        for (String str : merged_strings)
+            System.out.print(str + " ");
+        System.out.println();
 
         System.out.println();
         sc.close();
